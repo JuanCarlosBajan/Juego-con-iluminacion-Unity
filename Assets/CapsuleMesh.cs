409e008ead62080e2eb35b5a3a27a5a5ecc01f41
juan carlos bajan <baj20109@uvg.edu.gt>
2021-03-08 17:06:40 -0600

    Juego

---
+++ Assets/CapsuleMesh.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CapsuleMesh : MonoBehaviour
+{
+
+    public Light light;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (light.gameObject.activeSelf == true) {
+
+            light.intensity =  Mathf.Abs(Mathf.Sin(Time.time)) * 40;
+
+        }
+        
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        light.gameObject.SetActive(true);
+        
+    }
+}
---
+++ Assets/Scripts/Character.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+
+    public Text texto;
+    public GameObject panel;
+    Light light;
+    AudioSource audio;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        light = gameObject.GetComponent<Light>();
+        audio = gameObject.GetComponent<AudioSource>();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        Ray myray = Camera.main.ViewportPointToRay(new Vector3(0.5f, 0.5f));
+        RaycastHit hitinfo;
+
+        if (Input.GetMouseButtonDown(0)){
+            audio.Play();
+            if (light.intensity > 0)
+            {
+
+                light.intensity = 0;
+                
+            }
+            else {
+
+                light.intensity = 10;
+            }
+        }
+
+        if (Physics.Raycast(myray, out hitinfo) && hitinfo.collider.gameObject.CompareTag("ReadableObject"))
+        {
+            panel.SetActive(true);
+            texto.gameObject.SetActive(true);
+            texto.enabled = true;
+            texto.text = hitinfo.collider.name;
+        }
+        else {
+            panel.SetActive(false);
+            texto.gameObject.SetActive(false);
+            texto.text = "";
+
+        }
+
+    }
+
+
+}
